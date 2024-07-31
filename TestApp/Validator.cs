ddd37f6a8b0c7c7bbdd64bebd9de4ac1b7a96d84
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2024-07-31 14:23:43 -0700

    Created IsWithinRange validation

---
+++ TestApp/Validator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public static class Validator
+    {
+        public static bool IsWithinRange(int value, int min, int max)
+        {
+            return value >= min && value <= max;
+        }
+    }
+}
